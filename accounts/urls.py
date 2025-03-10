f3bdd206d5c9da664426a6bf783804c4b129adff
Brad Akemi <mirado.akemi@gmail.com>
2025-03-10 22:04:15 +0300

    acc

---
+++ accounts/urls.py
@@ -0,0 +1,14 @@
+# accounts/urls.py
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import UserProfileViewSet, UserSessionViewSet, UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'profiles', UserProfileViewSet)
+router.register(r'sessions', UserSessionViewSet, basename='session')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
