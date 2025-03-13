44f6270f859d200a7f805e943de4a07c96f234b4
Brad Akemi <mirado.akemi@gmail.com>
2025-03-13 14:33:14 +0300

    api

--- api/urls.py
+++ api/urls.py
@@ -10,17 +10,20 @@ from .views import ApiRootView, CustomTokenObtainPairView, api_user_info
 
 # API router
 router = DefaultRouter()
+from accounts import views
 
 urlpatterns = [
     # API root
-    path('', ApiRootView.as_view(), name='api-root'),
+    # path('', ApiRootView.as_view(), name='api-root'),
     
     # Authentication
     path('auth/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('auth/user/', api_user_info, name='api_user_info'),
-    
+    path('auth/register/initiate/', views.initiate_registration, name='initiate_registration'),
+    path('auth/register/complete/', views.complete_registration, name='complete_registration'),
+
     # Documentation
     path('schema/', CachedSpectacularAPIView.as_view(), name='schema'),
     path('docs/', ProtectedSpectacularSwaggerView.as_view(url_name='schema'), name='schema-swagger-ui'),
