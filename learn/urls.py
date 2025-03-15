d3b558ce6cf1dede1c449de7040ac79738fb7564
Brad Akemi <mirado.akemi@gmail.com>
2025-03-15 20:30:40 +0300

    utils

---
+++ learn/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from learn.views import *
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet)
+router.register(r'modules', ModuleViewSet)
+router.register(r'user/progress', UserProgressViewSet, basename='user-progress')
+router.register(r'user/certifications', CertificationViewSet, basename='user-certifications')
+router.register(r'user/points/transactions', PointsTransactionViewSet, basename='points-transactions')
+
+admin_router = DefaultRouter()
+admin_router.register(r'courses', AdminCourseViewSet)
+admin_router.register(r'modules', AdminModuleViewSet)
+admin_router.register(r'content-items', AdminContentItemViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('user/points/', UserPointsView.as_view(), name='user-points'),
+    path('admin/reference-data/', AdminReferenceDataView.as_view(), name='admin-reference-data'),
+]
