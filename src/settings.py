a3e534e25c4ab7c90a3e7c37b403872358263aeb
Brad Akemi <mirado.akemi@gmail.com>
2025-03-17 14:21:01 +0300

    add ai to settings

--- src/settings.py
+++ src/settings.py
@@ -62,6 +62,7 @@ INSTALLED_APPS = [
     'crispy_tailwind',
     
     # Hackitech apps
+    'ai',
     'accounts',
     'learning',
     'challenges',
@@ -192,8 +193,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=11),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 }
