0faf652cbfc158dc70261615a154a17822a19510
Brad Akemi <mirado.akemi@gmail.com>
2025-03-22 17:41:49 +0300

    ctf app

---
+++ ctf/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from ctf.models import *
+
+# Register your models here.
+admin.site.register(UserChallengeInstance)
+admin.site.register(Challenge)
+admin.site.register(ChallengeType)
