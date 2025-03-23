34c8d7df7894559c178ec65684b4e1fb28723cf6
Brad Akemi <mirado.akemi@gmail.com>
2025-03-23 11:54:59 +0300

    urls ctf

--- ctf/urls.py
+++ ctf/urls.py
@@ -4,6 +4,12 @@ from rest_framework.routers import DefaultRouter
 
 from .views import *
 
+router = DefaultRouter()
+router.register(r'challenges', ChallengeViewSet, basename='challenge')
+
 urlpatterns = [
-    path('start/<str:challenge_id>/', start_challenge, name='start_challenge'),
+    path('start/<uuid:challenge_id>/', start_challenge, name='start_challenge'),
+    path('status/<uuid:instance_id>/', check_status, name='check_status'), 
+    path('instances/<str:instance_id>/download-key/', download_ssh_key, name='download-ssh-key'),
+    path('', include(router.urls)),
 ]
