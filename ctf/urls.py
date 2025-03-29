21c9ab136d028e446fc83b0e574bcb38a2160dee
Brad Akemi <mirado.akemi@gmail.com>
2025-03-29 11:23:08 +0300

    urls

--- ctf/urls.py
+++ ctf/urls.py
@@ -5,11 +5,17 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
-router.register(r'challenges', ChallengeViewSet, basename='challenge')
+router.register(r'challenge-types', ChallengeTypeViewSet)
+router.register(r'docker-templates', DockerConfigTemplateViewSet)
+router.register(r'challenges', ChallengeViewSet)
+router.register(r'categories', ChallengeCategoryViewSet)
+
+
 
 urlpatterns = [
     path('start/<uuid:challenge_id>/', start_challenge, name='start_challenge'),
     path('status/<uuid:instance_id>/', check_status, name='check_status'), 
     path('instances/<str:instance_id>/download-key/', download_ssh_key, name='download-ssh-key'),
+    path('docker-templates/', docker_templates_view, name='docker-templates'),
     path('', include(router.urls)),
 ]
